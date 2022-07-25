3a1f2e42372b9b575d42fad8d32c5b9d83d5ac75
Parthasarathy <parthaa235@gmail.com>
2022-07-25 15:45:24 +0530

    updated

--- test/test.ino
+++ test/test.ino
@@ -5,8 +5,8 @@ void setup(){
 }
 void loop(){
     
-    digitalWrite(13,1);
+    digitalWrite(13,true);# here i had wriiten "HIGH" as "1", we can also write as "true"
     delay(500); 
-    digitalWrite(13,0);
+    digitalWrite(13,false);# here i had wriiten "LOW" as "0", we can also write as "false"
     delay(500);
 }
